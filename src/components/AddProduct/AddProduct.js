65af887593383fe206383ec781544fb8ad26d2fb
Prohor722 <prohor.b722@gmail.com>
2022-05-08 15:46:42 +0600

    products added successfully

--- src/components/AddProduct/AddProduct.js
+++ src/components/AddProduct/AddProduct.js
@@ -1,38 +1,153 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlusCircle } from "@fortawesome/free-solid-svg-icons";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
+import Loading from "../Shared/Loading";
 
 const AddProduct = () => {
-    const addProduct = (e) =>{
-        e.preventDefault();
+  const [product, setProduct] = useState({});
+  const [user, loading, error] = useAuthState(auth);
 
+  if (loading) {
+    return <Loading />;
+  }
+  if (error) {
+    console.log(error.message);
+  }
+
+  const addProduct = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const category = e.target.category.value;
+    const description = e.target.description.value;
+    const belt = e.target.belt.value;
+    const img = e.target.img.value;
+    const price = e.target.price.value;
+    const quantity = e.target.quantity.value;
+    const supplier = e.target.supplier.value;
+    const email = user.email;
+    console.log(product);
+
+    setProduct({
+      name,
+      category,
+      belt,
+      img,
+      price,
+      description,
+      quantity,
+      supplierName: supplier,
+      email,
+    });
+
+    if (product.name) {
+      const url = "http://localhost:5000/product";
+      fetch(url, {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(product),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data) {
+            alert("Your product have been added.");
+          }
+        });
     }
-    return (
-        <div>
-            <div className='col-md-4  mx-auto'>
-                <h1 className='my-5'>Add Product</h1>
-                <form className='mx-auto my-3' onSubmit={addProduct}>
-                    <div className="mb-3">
-                        <input type="text" className="form-control" name="name" placeholder='Enter Name or Brand of the Product' required/>
-                    </div>
-                    <div className="mb-3">
-                        <textarea type="text" className="form-control" name='category' placeholder='Give a short description' required/>
-                    </div>
-                    <div className="mb-3">
-                        <input type="number" className="form-control" name='price' placeholder='Price of the Product' required/>
-                    </div>
-                    <div className="mb-3">
-                        <input type="number" className="form-control" name='quantity' placeholder='Quantity of the product' required/>
-                    </div>
-                    <div className="mb-3">
-                        <input type="number" className="form-control" name='supplier' placeholder='Name of the supplier' required/>
-                    </div>
-
-                    <button type="submit" className="btn btn-dark">Add Product <FontAwesomeIcon icon={faPlusCircle}/></button>
-                </form>
+  };
+  return (
+    <div>
+      <div className="col-md-4  mx-auto">
+        <h1 className="mt-5 mb-3">Add Product</h1>
+        <form className="mx-auto my-3 row" onSubmit={addProduct}>
+          <div className="mb-3">
+            <input
+              type="text"
+              className="form-control"
+              name="name"
+              placeholder="Enter Name or Brand of the Product"
+              required
+            />
+          </div>
+          <div className="col-md-6">
+            <div className="mb-3">
+              <input
+                type="text"
+                className="form-control"
+                name="category"
+                placeholder="Give a category"
+                required
+              />
             </div>
-        </div>
-    );
+            <div className="mb-3">
+              <input
+                type="text"
+                className="form-control"
+                name="belt"
+                placeholder="What type of belt"
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <input
+                type="text"
+                className="form-control"
+                name="supplier"
+                placeholder="Name of the supplier"
+                required
+              />
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="mb-3">
+              <input
+                type="text"
+                className="form-control"
+                name="img"
+                placeholder="Give an image link here"
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <input
+                type="number"
+                className="form-control"
+                name="price"
+                placeholder="Price of the Product"
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <input
+                type="number"
+                className="form-control"
+                name="quantity"
+                placeholder="Quantity of the product"
+                required
+              />
+            </div>
+          </div>
+
+          <div className="mb-3">
+            <textarea
+              type="text"
+              className="form-control"
+              name="description"
+              placeholder="Give a short description"
+              required
+            />
+          </div>
+
+          <button type="submit" className="btn btn-dark">
+            Add Product <FontAwesomeIcon icon={faPlusCircle} />
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 // supplier
 export default AddProduct;
