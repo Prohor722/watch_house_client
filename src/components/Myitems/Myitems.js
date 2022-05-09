fbe88c088b0b40cb792d91dc5afbc2e319c4b82f
Prohor722 <prohor.b722@gmail.com>
2022-05-09 23:05:28 +0600

    my items no items info show done

--- src/components/Myitems/Myitems.js
+++ src/components/Myitems/Myitems.js
@@ -1,59 +1,69 @@
-import { faInfo, faPlus, faTrash } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { useEffect, useState } from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { useNavigate } from 'react-router-dom';
-import auth from '../../firebase.init';
-import Loading from '../Shared/Loading';
-
+import { faInfo, faPlus, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
+import auth from "../../firebase.init";
+import Loading from "../Shared/Loading";
 
 const Myitems = () => {
-    const [user, loading, error] = useAuthState(auth);
-    const [ products ,setProducts ] = useState([]);
-    const navigate = useNavigate();
-    
-    useEffect(()=>{
-        fetch(`https://fathomless-dawn-99199.herokuapp.com/products/?email=${user.email}`)
-        .then(res=>res.json())
-        .then(data=>setProducts(data))
-    },[user])    
+  const [user, loading, error] = useAuthState(auth);
+  const [products, setProducts] = useState([]);
+  const navigate = useNavigate();
 
-    if(loading){
-        return <Loading/>;
-    }
-    if(!products.length){ 
-      return <Loading/>
-    }
-    
-    if(error){
-        console.log(error.message);
-    }
+  useEffect(() => {
+    fetch(
+      `https://fathomless-dawn-99199.herokuapp.com/products/?email=${user.email}`
+    )
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, [user]);
 
-    const details=(id)=>{
-        navigate(`/product/${id}`)
-      }
-    
-    const productDelete = (id) =>{
-      const proceed = window.confirm("Are you sure?");
-        if(proceed){
-            const url = `https://fathomless-dawn-99199.herokuapp.com/product/${id}`;
-            fetch(url, {
-                method: "DELETE"
-            })
-            .then(res=>res.json())
-            .then(data=>{
-                console.log(data);
-                const remaning = products.filter(product=>id!==product._id);
-                setProducts(remaning);
-            })
-        }
-      }
-
+  if (loading) {
+    return <Loading />;
+  }
+  if(!products.length && !loading) {
     return (
-        <div>
+      <div className="mt-5" style={{height: 166}}>
+        <h3>Add some data to show.</h3>
+      </div>
+    );
+  }
+
+  if (error) {
+    console.log(error.message);
+  }
+
+  const details = (id) => {
+    navigate(`/product/${id}`);
+  };
+
+  const productDelete = (id) => {
+    const proceed = window.confirm("Are you sure?");
+    if (proceed) {
+      const url = `https://fathomless-dawn-99199.herokuapp.com/product/${id}`;
+      fetch(url, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          const remaning = products.filter((product) => id !== product._id);
+          setProducts(remaning);
+        });
+    }
+  };
+
+  return (
+    <div>
       <h1 className="mt-5 mb-3">My inventory</h1>
       <div className="d-flex me-5 pe-5">
-        <button className="btn btn-dark ms-auto" onClick={()=>navigate('/addProduct')}>Add <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon></button>
+        <button
+          className="btn btn-dark ms-auto"
+          onClick={() => navigate("/addProduct")}
+        >
+          Add <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
+        </button>
       </div>
       <hr />
       <table className="table">
@@ -67,29 +77,42 @@ const Myitems = () => {
           </tr>
         </thead>
         <tbody>
-          
-            { products.map((product) => (
-                <tr key={product._id}>
+          {products.map((product) => (
+            <tr key={product._id}>
+              <td>
+                <img
+                  className="img-fluid item-img"
+                  src={product.img}
+                  alt="product"
+                />
+              </td>
 
-                    <td><img className="img-fluid item-img" src={product.img}
-                    alt="product"/></td>
-                    
-                    <td>{product.name}</td>
-                    
-                    <td>${product.price}</td>
+              <td>{product.name}</td>
 
-                    <td>{product.quantity}</td>
+              <td>${product.price}</td>
 
-                    <td>
-                        <button onClick={()=>details(product._id)} className="btn btn-secondary me-2 px-3"><FontAwesomeIcon icon={faInfo}/></button>
-                        <button onClick={()=>productDelete(product._id)} className="btn btn-danger"><FontAwesomeIcon icon={faTrash}/></button>
-                    </td>
-              </tr>
-            ))}
+              <td>{product.quantity}</td>
+
+              <td>
+                <button
+                  onClick={() => details(product._id)}
+                  className="btn btn-secondary me-2 px-3"
+                >
+                  <FontAwesomeIcon icon={faInfo} />
+                </button>
+                <button
+                  onClick={() => productDelete(product._id)}
+                  className="btn btn-danger"
+                >
+                  <FontAwesomeIcon icon={faTrash} />
+                </button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
-    );
+  );
 };
 
 export default Myitems;
